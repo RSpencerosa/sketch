fa52e8e0173ca49872fa9ffdb6323cd2f1f5dc6e
Rhiannon <154023480+RSpencerosa@users.noreply.github.com>
2025-08-09 18:34:56 -0700

    Add new page button

--- script.js
+++ script.js
@@ -1,11 +1,30 @@
 
-
+const btn = document.querySelector("button")
 const container = document.querySelector("#container")
 
+btn.addEventListener("click", function() {
+    let input = prompt("New size?")
+    Number(input)
+    if (input > 0 && input < 100) {
+        remove()
+        reset(input)
+    } else {
+        alert("Error! Please type one number below 100!")
+    }
+})
 
-for (i = 0; i < 255; i++) {
-    const square = document.createElement("div")
-    square.classList.add("square")
-    square.addEventListener("mouseover",()=>square.style.cssText="background: black")
-    container.appendChild(square)
+function remove() {
+    const children = document.querySelectorAll(".square")
+    children.forEach((child)=>container.removeChild(child))
 }
+
+function reset(number) {
+    for (i = 0; i < number*number; i++) {
+        const square = document.createElement("div")
+        square.classList.add("square")
+        square.addEventListener("mouseover",()=>square.style.cssText="background: black")
+        container.appendChild(square)
+    }
+}
+
+reset(255)
